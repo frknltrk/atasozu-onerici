013b44f0d299e292c788d5163aae49f02847692b
Furkan Ünlütürk <unluturkfurkan@protonmail.com>
2022-06-21 02:43:37 +0300

    add cache mech.

--- app.py
+++ app.py
@@ -3,30 +3,40 @@ import streamlit as st
 import pandas as pd
 from sentence_transformers import SentenceTransformer, util
 
-model = SentenceTransformer("dbmdz/bert-base-turkish-uncased")
-embeddings = torch.load("proverb_embeddings.pt")
-df = pd.read_csv("atasozleri-vk.csv", index_col=[0], na_filter=False)
-
 st.set_page_config(
-    page_title="Türkçe Atasözü Önerici",
-    page_icon="🎈",
+  page_title="Türkçe Atasözü Önerici",
+  page_icon="🎈",
 )
+
+@st.cache
+def load_model(allow_output_mutation=True):
+  return SentenceTransformer("dbmdz/bert-base-turkish-uncased")
+
+@st.cache
+def load_embeddings():
+  return torch.load("proverb_embeddings.pt")
+
+@st.cache
+def load_data():
+  return pd.read_csv("atasozleri-vk.csv", index_col=[0], na_filter=False)
+
+model = load_model()
+embeddings = load_embeddings()
+df = load_data()
 
 st.title("Türkçe Atasözü Önerici")
 st.subheader("BERT Tabanlı Atasözü Öneri Sistemi")
 
 girdi = st.text_area(
-    "Aklından geçenleri buraya dök."
+  "Aklından geçenleri buraya dök."
 )
 
 if girdi:
-    girdi_vek = model.encode(girdi)
-    sim_vec = util.cos_sim(girdi_vek, embeddings)[0]
-    best_indices = sim_vec.topk(5).indices
+  girdi_vek = model.encode(girdi)
+  sim_vec = util.cos_sim(girdi_vek, embeddings)[0]
+  best_indices = sim_vec.topk(5).indices
     
-    results = df[['title', 'anlam']].iloc[best_indices]
+  results = df[['title', 'anlam']].iloc[best_indices]
 
-    st.subheader("Öneriler")
-    st.write(results)
-
-
+  st.subheader("Öneriler")
+  st.write(results)
--- pp-script.py
+++ pp-script.py
@@ -4,7 +4,7 @@ from sentence_transformers import SentenceTransformer
 model = SentenceTransformer('dbmdz/bert-base-turkish-uncased')
 
 df = pandas.read_csv('atasozleri-vk.csv', index_col=[0], na_filter=False)
-list_of_tokens = df['yeni_parcalar'].apply(lambda t_list: t_list.replace(', ',' ')).astype(str).values
+list_of_tokens = df['yeni_parcalar'].apply(lambda tokens: tokens.replace(', ',' ')).astype(str).values
 
 # https://www.sbert.net/docs/package_reference/SentenceTransformer.html#sentence_transformers.SentenceTransformer.encode
 embeddings = model.encode(list_of_tokens,
